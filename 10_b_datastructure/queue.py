bedfa9d9e66f5894a62a3afe48834be7512ec241
patrick <873948000@qq.com>
2017-03-26 13:47:13 +0800

    Chapter 10: Basic Data Structure     10.1 Stack & Queue

---
+++ 10_b_datastructure/queue.py
@@ -0,0 +1,35 @@
+class Queue:
+
+    def __init__(self, size):
+        self.Q = [None for i in range(size + 1)]
+        self.size = size + 1
+        self.head = 0
+        self.tail = 0
+
+    def enqueue(self, elem):
+        if (self.tail + 1) % self.size == self.head:
+            print('overflow!')
+            return
+        self.Q[self.tail] = elem
+        self.tail = (self.tail + 1) % self.size
+
+    def dequeue(self):
+        if self.tail == self.head:
+            print('underflow')
+            return
+        elem = self.Q[self.head]
+        self.Q[self.head] = None
+        self.head = (self.head + 1) % self.size
+        return elem
+
+if __name__ == '__main__':
+    Q = Queue(3)
+    a = Q.dequeue()
+    Q.enqueue(1)
+    Q.enqueue(2)
+    Q.enqueue(3)
+    Q.enqueue(4)
+    for i in Q.Q:
+        print(i)
+    b = Q.dequeue()
+    print(b)
---
+++ 10_b_datastructure/stack.py
@@ -0,0 +1,41 @@
+class Stack:
+
+    def __init__(self, size):
+
+        self.S = [None for i in range(size)]
+        self.top = 0
+        self.size = size
+
+    def stack_empty(self):
+        if self.S[self.top]:
+            return False
+        return True
+
+    def push(self, elem):
+        if self.top + 1 >= self.size:
+            print('overflow!')
+            return
+        if not self.stack_empty():
+            self.top += 1
+        self.S[self.top] = elem
+
+    def pop(self):
+        if self.stack_empty():
+            print('underflow!')
+            return
+        elem = self.S[self.top]
+        self.S[self.top] = None
+        self.top -= 1
+        return elem
+
+if __name__ == '__main__':
+    S = Stack(3)
+    a = S.pop()
+    S.push(1)
+    S.push(2)
+    S.push(3)
+    S.push(4)
+    b = S.pop()
+    for i in S.S:
+        print(i)
+
